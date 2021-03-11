08daba2b67e7c33ab88ce471e4d2fc7834b4b497
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2021-03-11 21:33:14 +0100

    Add permissions

--- Android.mk
+++ Android.mk
@@ -1,5 +1,14 @@
 LOCAL_PATH := $(call my-dir)
 
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := privapp-permissions-org.codeaurora.snapcam.privileged.xml
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
 include $(CLEAR_VARS)
 LOCAL_MODULE := GcamSailfish
 LOCAL_MODULE_TAGS := optional
@@ -9,4 +18,5 @@ LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_MODULE_PATH := $(TARGET_OUT)/priv-app
 LOCAL_OVERRIDES_PACKAGES := SnapdragonCamera Camera2 Gallery2
+LOCAL_REQUIRED_MODULES := privapp-permissions-org.codeaurora.snapcam.privileged.xml
 include $(BUILD_PREBUILT)
